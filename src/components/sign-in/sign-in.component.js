505668bd2d2540b2ce8c6f8fa353e56f107e5f88
Nicholas Giusti <nicholas.giusti89@gmail.com>
2020-10-01 11:20:01 -0700

    finished building sign in component

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./sign-in.styles.scss";
 
@@ -16,12 +16,18 @@ export default class SignIn extends Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({
-      email: "",
-      password: "",
-    });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: "",
+        password: "",
+      });
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   handleChange = event => {
@@ -47,6 +53,7 @@ export default class SignIn extends Component {
           />
           <FormInput
             name='password'
+            type='password'
             value={this.state.password}
             required
             label='Password'
